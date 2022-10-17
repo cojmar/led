1c0186dee94b7c3a4cbdaf7300a374986d6ae70b
cojmar <cojmar@gmail.com>
2022-10-17 03:47:00 +0300

    ok

--- src/main.js
+++ src/main.js
@@ -12,12 +12,7 @@ document.addEventListener('DOMContentLoaded', () => new class {
 		let container = document.querySelector('.container')
 		container.innerHTML = ''
 
-		let routes = Object.getOwnPropertyNames(Object.getPrototypeOf(this)).reduce((r, i) => {
-			if (i.indexOf('page_') === 0) r.push(i)
-			return r
-		}, [])
-
-		let route = window.location.hash.substring(1) || routes[0].replace('page_', '')
+		let route = window.location.hash.substring(1) || 'projects_list'
 		let template = document.querySelector(`#${route}`)
 
 		let route_found = 0
@@ -27,17 +22,15 @@ document.addEventListener('DOMContentLoaded', () => new class {
 			container.appendChild(item)
 			route_found++
 		}
-		if (~routes.indexOf(`page_${route}`)) {
+		if (typeof this[`page_${route}`] === 'function') {
 			this[`page_${route}`]()
 			route_found++
 		}
-
 
 		Array.from(document.querySelectorAll('.menu-item > a')).map(a => {
 			if (a.getAttribute('href').substring(1) === route) a.classList.add('selected')
 			else a.classList.remove('selected')
 		})
-
 
 		// route not found
 		if (route_found === 0) container.innerHTML = '<center><b>Route not found!</b></center>'
@@ -52,6 +45,7 @@ document.addEventListener('DOMContentLoaded', () => new class {
 
 	// pages
 	async page_projects_list() {
+
 		let data = await this.get_projects()
 
 		let dom = {
---
+++ src/server/index.php
@@ -0,0 +1,65 @@
+<?php
+session_start();
+class router
+{
+    //==Routes
+
+    function projects_list()
+    {
+        $out = $this->server_model->projects_list();
+        json_output($out);
+    }
+
+
+
+    //==Custom Init
+    private function init()
+    {
+        $this->db = new db_class();
+        $this->server_model = new server_model();
+        $this->db->connect("localhost", "root", "asdasd", "led_panel");
+    }
+
+    function __construct()
+    {
+        set_time_limit(0);
+        $route = (!empty($_GET['route'])) ? $_GET['route'] : '';
+        //==Run custom init
+        if (method_exists($this, 'init')) $this->init();
+        //==Run route
+        if (method_exists($this, $route)) $this->$route();
+    }
+}
+//==Autoload
+spl_autoload_register(function ($class_name) {
+    include  $class_name . '.php';
+});
+//==Run
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+set_time_limit(0);
+function json_output($data = false)
+{
+    $out = (is_array($data) || is_object($data)) ? json_encode($data) : json_encode(array($data));
+    header('Content-Type: application/json');
+    die($out);
+}
+
+$start_time = microtime(true);
+ob_start();
+new router();
+$output = ob_get_contents();
+$execution_time = microtime(true) - $start_time;
+@ob_end_clean();
+print "
+		<style>
+			body, html{
+				margin:1px 3px;
+				padding:0px;
+			}
+		</style>
+		<div style='background-color:#5d5d5d;padding:5px;border:1px solid #5d5d5d;color:fff;font-size:14px;height:10px;line-height:10px;'>
+			Execution time: {$execution_time} seconds
+		</div>
+	";
+die($output);
